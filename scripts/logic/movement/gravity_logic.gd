78ea3ca48d5894083789f800c33c67eac4965749
TimurTimergalin <timur20041229@gmail.com>
2025-10-02 01:33:57 +0300

    closes #30: разделил ускорение свободного падения

--- scripts/logic/movement/gravity_logic.gd
+++ scripts/logic/movement/gravity_logic.gd
@@ -7,4 +7,9 @@ class_name GravityLogic
 
 
 func _physics_process(delta: float) -> void:
+	gravity_store.gravity = (
+		gravity_store.free_fall_decceleration
+		if target.velocity.y > 0
+		else gravity_store.free_fall_acceleration
+	)
 	target.velocity.y += gravity_store.gravity * delta
